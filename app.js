f3ef4a2ea6e97c395d3bfc9bbdec59bc658efb94
srinathsr <46624510+srinathsr@users.noreply.github.com>
2019-09-15 19:07:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import HelloWorld from '/home/srinath/Desktop/important/hello-world/components/HelloWorld';
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+
